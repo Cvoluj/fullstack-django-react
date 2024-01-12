9ebc85a6456fbe41e0868b9dd3b5b74216793425
cvoluj <berezanskiydaniiil@gmail.com>
2024-01-12 17:27:50 +0200

    user crud, router

---
+++ core/routers.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from core.user.viewsets import UserViewSet
+
+router = routers.SimpleRouter()
+
+router.register(r'user', UserViewSet, basename='user')
+
+urlpatterns = [
+    *router.urls,
+]
