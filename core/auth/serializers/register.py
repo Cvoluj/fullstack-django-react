e6a4255bc3410f41642cf0b44eab73fcc8c2a7d3
cvoluj <berezanskiydaniiil@gmail.com>
2024-01-14 03:16:38 +0200

    added .env.sample

--- core/auth/serializers/register.py
+++ core/auth/serializers/register.py
@@ -3,5 +3,17 @@ from rest_framework import serializers
 from core.user.serializers import UserSerializer
 from core.user.models import User
 
+
 class RegisterSerializer(UserSerializer):
-    pass
+    """
+    Registration serializer for requests and user creation
+    """
+
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- core/routers.py
+++ core/routers.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-
+from core.auth.viewsets.register import RegisterViewSet
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 
 urlpatterns = [
     *router.urls,
