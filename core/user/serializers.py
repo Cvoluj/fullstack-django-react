27a63f8faa1573ef4db580a8de43d27823d9f57b
cvoluj <berezanskiydaniiil@gmail.com>
2024-01-21 02:54:21 +0200

    crud for Post, custom permission for users

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,13 +1,10 @@
 from rest_framework import serializers
 
+from core.abstract.serializers import AbstractSerializer
 from core.user.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
-
+class UserSerializer(AbstractSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name',
